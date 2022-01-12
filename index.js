86565a3483e96472079e77db51416549ccc4a1b3
callumfowler101 <callumfowler101@gmail.com>
2022-01-12 15:04:33 +0000

    added create command

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const dialogflow = require('@google-cloud/dialogflow');
 const intentClient = new dialogflow.IntentsClient();
 
-const projectID = 'apitestagent-bmmn';
-const intentConfig = {
-    id: projectID,
-    client: intentClient,
-    displayName: "thankYou_intent"
-}
-
 const createIntent = async ({id, client, displayName}) => {
     const agentPath = client.projectAgentPath(id);
     const intent = {
@@ -19,7 +12,26 @@ const createIntent = async ({id, client, displayName}) => {
     }
 
     const [response] = await client.createIntent(createIntentReq);
+    console.log(`Successfully created ${intent.displayName} intent.`)
 }
 
+const runApp = () => {
+    const projectID = 'apitestagent-bmmn';
+    const command = process.argv[2];
+    const displayName = process.argv[3];
+    
+    if(command==="create-intent"){
+        const intentConfig = {
+            id: projectID,
+            client: intentClient,
+            displayName: displayName  
+        }
+        createIntent(intentConfig);
+    } else {
+        console.log(`Error ${command} is not a valid command`);
+    }
+}
 
-createIntent(intentConfig);
+runApp();
+
+
